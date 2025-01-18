e519e7116fec1df3441fa066ce4f7aa5538c3f94
Mayra Landa <mlandae16@gmail.com>
2025-01-17 19:08:45 -0800

    add index.js

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const routes = require("./routes");
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(passport.initialize());
+require("./config/passport");
+
+app.use("./api", routes);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
