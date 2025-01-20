d2dd6b8805d4a5e7b480d89b80a076b1c4968611
Mayra Landa <mlandae16@gmail.com>
2025-01-20 12:54:09 -0800

    add passport.js

---
+++ middleware/passport.js
@@ -0,0 +1,24 @@
+const {Strategy: JwtStrategy, ExtractJwt} = require("passport-jwt");
+const prisma = require("../prisma/client");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: JWT_SECRET,
+}
+
+const strategy = new JwtStrategy(options, async (payload, done) => {
+  try {
+    const user = await prisma.user.findUnique({where: {id: payload.id}}); 
+    if (!user) return done(null, false);
+
+    return done(null, user);
+  } catch (error){
+    return done(error, false);
+  }
+})
+
+module.exports = (passport) => {
+  passport.use(strategy);
+}
