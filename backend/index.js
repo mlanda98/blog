924168bb2c71e206d9a84456f88ffbd1a6d0a27e
Mayra Landa <mlandae16@gmail.com>
2025-01-31 18:33:25 -0800

    add login and register routes to index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(passport.initialize());
 
@@ -25,7 +25,13 @@ app.use("/auth", authRoutes);
 app.use("/posts", postRoutes);
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
